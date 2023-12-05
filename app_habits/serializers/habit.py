7f63fb69c393fe1a1d20e2a9140dc5244c728541
u_alex90 <u_alex90@mail.ru>
2023-12-06 01:03:52 +1000

    Add validators

--- app_habits/serializers/habit.py
+++ app_habits/serializers/habit.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
 from app_habits.models import Habit
-from validators.general_validators import FillingNotOutTwoFieldsValidator
-from app_habits.validators.habit import TimeToCompleteValidator
+from validators.general_validators import FillingNotOutTwoFieldsValidator, FieldsIsNone
+from app_habits.validators.habit import (
+    TimeToCompleteValidator,
+    RelatedHabitOnlyNiceValidator,
+)
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -15,24 +18,29 @@ class HabitNiceCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         exclude = ('start_time', 'periodicity', 'related_habit', 'reward')
-        read_only_fields = ('owner', 'is_nice_habit')
-        validators = []
+        read_only_fields = ('owner', 'is_nice')
+        validators = [
+            # Проверяем что время выполнения не превышает 120 секунд
+            TimeToCompleteValidator('time_to_complete'),
+        ]
 
 
 class HabitGoodCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
-        read_only_fields = ('owner', 'is_nice_habit')
+        read_only_fields = ('owner', 'is_nice')
         validators = [
             # Проверяем что одновременно не указаны связанная привычка и вознаграждение
             FillingNotOutTwoFieldsValidator('related_habit', 'reward'),
+            # Проверяем что время выполнения не превышает 120 секунд
             TimeToCompleteValidator('time_to_complete'),
+            # Проверяем что в связанных привычках, привычка с признаком приятной
+            RelatedHabitOnlyNiceValidator('related_habit'),
         ]
 
 
 class HabitListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
-        fields = ('task', 'start_time', 'location', 'periodicity', 'is_nice_habit')
-
+        fields = ('task', 'start_time', 'location', 'periodicity', 'is_nice')
