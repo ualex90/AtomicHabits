2b60b25581c5b27fa714665d624799355f36b832
u_alex90 <u_alex90@mail.ru>
2023-12-06 01:03:24 +1000

    Add FieldsIsNoneValidator

--- validators/general_validators.py
+++ validators/general_validators.py
@@ -10,7 +10,22 @@ class FillingNotOutTwoFieldsValidator:
         self.second_field = second_field
 
     def __call__(self, value):
-        if dict(value).get(self.first_field) and dict(value).get(self.second_field):
+        if value.get(self.first_field) and value.get(self.second_field):
             if not self.message:
                 self.message = f'Недопустимо одновременно указывать "{self.first_field}" и "{self.second_field}"'
             raise serializers.ValidationError(self.message)
+
+
+class FieldsIsNoneValidator:
+    """ Валидатор проверят, что указанные поля не заполнены """
+
+    def __init__(self, *args, message=None):
+        self.message = message
+        self.fields = args
+
+    def __call__(self, value):
+        for field in self.fields:
+            if value.get(field):
+                if not self.message:
+                    self.message = f'Запрещено заполнять поле "{field}""'
+                raise serializers.ValidationError(self.message)
